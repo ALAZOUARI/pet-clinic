f30123804df1032b2edb6be3207d2408b8768669
Ala <ala.zouariii@gmail.com>
2020-11-01 17:05:37 +0100

    add PetType on Startup . Closes #24

--- pet-clinic-web/src/main/java/tuto/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/tuto/springframework/petclinic/bootstrap/DataLoader.java
@@ -2,24 +2,38 @@ package tuto.springframework.petclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import tuto.springframework.petclinic.model.Owner;
+import tuto.springframework.petclinic.model.PetType;
 import tuto.springframework.petclinic.model.Vet;
 import tuto.springframework.petclinic.services.OwnerService;
+import tuto.springframework.petclinic.services.PetTypeService;
 import tuto.springframework.petclinic.services.VetService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerservice ;
     private final VetService vetservice ;
-
-    public DataLoader(OwnerService ownerservice, VetService vetservice){
+    private final PetTypeService petTypeService;
+    public DataLoader(OwnerService ownerservice, VetService vetservice, PetTypeService petTypeService){
         this.ownerservice = ownerservice;
 
 
         this.vetservice = vetservice;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType =petTypeService.save(dog);
+
+
+        PetType cat= new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType =petTypeService.save(dog);
+
+
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Alaa");
         owner1.setLastName("ZOUARI");
@@ -44,6 +58,8 @@ public class DataLoader implements CommandLineRunner {
         vet2.setLastName("BOOBiiesv");
         vetservice.save(vet2);
         System.out.println("Loaded Vets ...");
+
+
 
 
 
