138bde78ad7744fb54f513a6759c903bbcfad1db
Ala <ala.zouariii@gmail.com>
2020-11-06 17:08:48 +0100

    Add Spring Data JPA to OwnerRepository .Closes#33

--- pet-clinic-data/src/main/java/tuto/springframework/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/tuto/springframework/petclinic/repositories/OwnerRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 import tuto.springframework.petclinic.model.Owner;
 @Repository
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
+    Owner findByLastName(String lastName);
 }
